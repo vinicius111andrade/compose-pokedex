a9fc3ba79b2fb7bd05fefc11ea2ecc306570aa48
vinicius111andrade <vinicius111andrade@gmail.com>
2023-12-24 10:52:38 -0300

    Setting up project

--- app/src/main/java/com/vdemelo/mycomposepokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/vdemelo/mycomposepokedex/ui/theme/Theme.kt
@@ -16,18 +16,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-//private val DarkColorScheme = darkColorScheme(
-//    primary = Purple80,
-//    secondary = PurpleGrey80,
-//    tertiary = Pink80
-//)
-//
-//private val LightColorScheme = lightColorScheme(
-//    primary = Purple40,
-//    secondary = PurpleGrey40,
-//    tertiary = Pink40
-//)
-
 private val DarkColorScheme = darkColorScheme(
     primary = Color.Yellow,
     background = Color(0xFF101010),
@@ -48,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun MyComposePokedexTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -60,6 +48,7 @@ fun MyComposePokedexTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
